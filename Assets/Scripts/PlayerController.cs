d9a796b2e7249fc1c00a565dfe0aac6dec3fab3a
Tyler Friedberg <thefriedberger@gmail.com>
2020-01-29 14:18:57 -0500

    Added script to pick ups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
-
+    private int score;
+    public Text scoreText;
+    public Text winText;
     public float speed;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetScoreText();
+        winText.text = "";
     }
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
@@ -18,5 +24,18 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Pick Up")) {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreText();
+        }
+    }
+     void SetScoreText() {
+        scoreText.text = "Score: " + score.ToString();
+        if(score >= 12) {
+            winText.text = "You Win";
+        }
     }
 }
